99e4ec24486544ad97f507d7483dbd79b70a881b
onaseer07 <onaseer07@gmail.com>
2018-10-17 21:40:55 -0500

    Working MERN

--- sortable-item-app/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  Category: require("./category"),
-  MenuItem: require("./menuItem"),
-};
--- sortable-item-app/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const categoryRoutes = require("./categories");
-const menuRoutes = require("./menuitems");
-
-
-router.use("/categories", categoryRoutes);
-router.use("/menuitems", menuRoutes);
-
-module.exports = router;
