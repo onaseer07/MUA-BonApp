a0cd056b6e92abceb095756f011858e28446ee8d
onaseer07 <37281993+onaseer07@users.noreply.github.com>
2018-12-19 22:47:07 -0600

    Update categories.js

--- client/src/Form/components/pages/Categories/categories.js
+++ client/src/Form/components/pages/Categories/categories.js
@@ -1,24 +1,24 @@
 import React, {Component,Fragment} from 'react';
 import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 import { Link, withRouter } from "react-router-dom";
-import Div from '../../../components/div';
-import Form from '../../../components/Form';
-import Button from '../../../components/button';
-import DeleteBtn from '../../../components/DeleteBtn';
-import Label from '../../../components/label';
-import Input from '../../../components/input';
+import Div from '../../components/div';
+import Form from '../../components/Form';
+import Button from '../../components/button';
+import DeleteBtn from '../../components/DeleteBtn';
+import Label from '../../components/label';
+import Input from '../../components/input';
 import './App.css';
-import API from '../../../../utils/API';
-
+import API from '../../../utils/API';
+import fire from '../../../config/Fire';
 
 
 //sortableItem function with value passed into it to call it within JSX
 const SortableItem = SortableElement(({value,onClick}) =>
   //{value} = arrayElements
-  <div className='col-lg-3 d-inline-block m'>
-    <button id={value._id} className='delete-btn float-left d-inline-block' name='editCat' onClick={onClick}>✎</button>
-    <button id={value._id} className='delete-btn float-right d-inline-block' name='deleteCat' onClick={onClick}>x</button> 
+  <div className='p-5'>
+    <button id={value._id} className='delete-btn float-left d-inline-block ml-5' name='editCat' onClick={onClick}>✎</button>
     <h1 key={value._id} className='shadow bg-light display-1 inline-block'>{value.category}</h1>
+    <button id={value._id} className='delete-btn float-right d-inline-block' name='deleteCat' onClick={onClick}>x</button> 
   </div>
 );
 
@@ -31,10 +31,11 @@ const SortableList = SortableContainer(({items,onClick}) =>
     </div>
 );
 
-//Class App that has methods and render method
+//Customize Categories Class
 class CustomCats extends Component {
   constructor(props){
     super(props)
+    this.logout = this.logout.bind(this);
     this.state = {
       //updating items with the 
       items: [],
@@ -52,17 +53,11 @@ class CustomCats extends Component {
       this.loadCats();
     }
 
-    //Load categories function
-    loadCats = () =>{
-      API.getCats()
-      .then(res =>{
-        console.log(res.data);
-        this.setState({items:res.data,menuItems:res.data})
-      })
-      .catch(err => console.log(err));
-    }
-    
-    //Saving Category function
+
+
+ /*------------------------------------------------------------------------------------------------------------------------------------------------*/  
+//CREATE MODULES(s)
+    //Adding Category function
     addCat = (e)=>{
       e.preventDefault();
       let category = this.state.category[0]
@@ -76,50 +71,19 @@ class CustomCats extends Component {
             ).then(res=> this.loadCats())
       e.target.reset();
     }
-    
-    //Delete Category by _id
-    deleteCat = (data)=>{
-      const {id} = data.target;
-      API.deleteCat(id)
-        .then(res=> this.loadCats())
-        .catch(err => console.log(err));
+ /*------------------------------------------------------------------------------------------------------------------------------------------------*/ 
+//READ MODULE(s):
+    //Load categories function
+    loadCats = () =>{
+      API.getCats()
+      .then(res =>{
+        console.log(res.data);
+        this.setState({items:res.data,menuItems:res.data})
+      })
+      .catch(err => console.log(err));
     }
-
-    //Edit Category by _id
-      editCat = (e)=>{
-        console.log(e.target.id)
-        this.props.history.push('/cust/' + e.target.id)
-        console.log(this.props) 
-      }
-      //Display Category
-      displayCat = ()=>{
-
-        return(
-        <Fragment>
-        <Div className='container'>
-          <Div className='row'>
-            <Div className='form-group'>
-              <Form onSubmit={this.addCat}>
-                <Label for='category'>Category:
-                <Input type='text' className='form-control'name='category' onChange={this.handleChange}/>
-                </Label> 
-                <Input id='submit' type='submit' value='+'/>
-              </Form>
-            </Div>
-          </Div>
-        </Div>
-        <Div className='container-fluid'>
-          <Div className='row d-flex justify-content-center align-content-center'>
-            <Div className='displayChg d-flex justify-content-center align-content-center'>
-            <SortableList axis='x' items={this.state.items} onSortEnd={this.onSortEnd} 
-              onClick={(data)=>{data.target.name === 'deleteCat' ? this.deleteCat(data):this.editCat(data)}}/>
-            </Div>
-          </Div>
-        </Div>
-        </Fragment>
-        )
-      }
-
+ /*------------------------------------------------------------------------------------------------------------------------------------------------*/
+ //UPDATE MODULES(s)
     //handleChange functions
     handleChange = data=>{
       const {name,value} = data.target;
@@ -138,11 +102,79 @@ class CustomCats extends Component {
     //logging the updated state to verify state position
     console.log(this.state.items,oldIndex,newIndex);
   };
+ /*------------------------------------------------------------------------------------------------------------------------------------------------*/
+ //DELETE MODULE(s)   
+    //Delete Category by _id
+    deleteCat = (data)=>{
+      const {id} = data.target;
+      //Make API Call to delete cat by passing in the id of the Category that user clicked
+      API.deleteCat(id)
+      //invoke the loadCats function
+        .then(res=> this.loadCats())
+        .catch(err => console.log(err));
+    }
+/*------------------------------------------------------------------------------------------------------------------------------------------------*/
+//CATEGORIES PAGE fn MODULE(s)
+    //Edit Category by _id
+      editCat = (e)=>{
+        console.log(e.target.id)
+        this.props.history.push('/cust/' + e.target.id)
+        console.log(this.props) 
+      }
+    //redirect to home
+      redirect = (e)=>{
+        console.log(e.target.id)
+        this.props.history.push('/')
+        console.log(this.props) 
+      }
+
+    //Logout
+      logout() {
+        fire.auth().signOut();
+    }
+
+      //Display Category
+      displayCat = ()=>{
+
+        return(
+        <Fragment>
+        <Div className='container'>
+        <Div className='row d-flex justify-content-center'>
+            <Div className='form-group'>
+              <Form className='form-inline p-3' onSubmit={this.addCat}>
+                <Label for='category mr-3'>Add <br/>Category:  
+                <Input type='text' className='form-control'name='category' onChange={this.handleChange}/>
+                </Label> 
+                <Input id='submit' type='submit' value='+'/>
+              </Form>
+            </Div>
+          </Div>
+        </Div>
+        <Div className='container-fluid'>
+          <Div className='row d-flex justify-content-center align-content-center'>
+            <Div className='displayChg d-flex justify-content-center align-content-center'>
+            <SortableList axis='x' items={this.state.items} onSortEnd={this.onSortEnd} 
+              onClick={(data)=>{data.target.name === 'deleteCat' ? this.deleteCat(data):this.editCat(data)}}/>
+            </Div>
+            </Div>
+        </Div>
+        </Fragment>
+        )
+      }
+
+
   render() {
 
     return (
 
       <Fragment>
+        <img className='img-fluid' src='mualogosm.jpg'/><br/>
+        <button onClick={this.redirect} className='logout mt-3'>
+          <i class="fas fa-home"></i><span>Home</span>
+        </button>
+        <button onClick={this.logout} className='logout mt-3'>
+          <i className="fas fa-user"></i><span>Logout</span>
+        </button>
         {this.displayCat()}
       </Fragment>
     )
@@ -150,27 +182,3 @@ class CustomCats extends Component {
 }
 
 export default withRouter(CustomCats);
-
-/*
-
-- CatDetailPage
-assign db_id to key prop (key prop = catDB_id) so admin can use onclick delete & edit function on each category. DONE
-
-  on delete remove category by invoking deleteCat(). - DONE
-
-  on click of pencil icon it will hit the route "/api/categories/:id" to render catdata and its menuItems
-    - create input with placholder = res.data[0].category with button to save ()=>{update catNamefunction} (NTH)
-
-    MenuItems FORM:
-
-    Create a form that allows user to capture user's menuItem input {itemName:"",itemDesc.:"",itemPic:"", itemPrice:"$", catId:catFK} 3
-      on menuItem form save it will submit the formdata to db creating new menuItem with cat_id = :/id 4
-        reload the data with catName, menuItems
-
-        Initial State = {
-          categories: [{...}] //DB DATA
-          items: [] //shuffling of menuItems as NTH
-        }
-
-
-*/
